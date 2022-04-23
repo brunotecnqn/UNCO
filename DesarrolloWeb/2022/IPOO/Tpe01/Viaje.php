9f9644982758db7df09da873aa53409bf7cb951b
brunotecnqn <91470629+brunotecnqn@users.noreply.github.com>
2022-04-22 22:51:05 -0300

    Entrega del Tpe Nº2
    Se modificó la clase Viaje para que ahora los pasajeros sean un objeto que tenga los atributos nombre, apellido, numero de documento y teléfono. El viaje ahora contiene una referencia a una colección de objetos de la clase Pasajero, también se  creó una clase Responsable, que es referenciada en la clase Viaje. volviendo a implementar y agregando nuevos métodos para que se pueden modificar los datos del viaje.

--- DesarrolloWeb/2022/IPOO/Tpe01/Viaje.php
+++ DesarrolloWeb/2022/IPOO/Tpe01/Viaje.php
@@ -7,14 +7,16 @@ class Viaje
    private $codigo_viaje;
    private $destino;
    private $cant_maxima;
+   private $objResponsable;
    private $arre_pasajero = array();
 
-   public function  __construct($codigo_viaje, $destino, $cant_maxima, &$arre_pasajero)
+   public function  __construct($codigo_viaje, $destino, $cant_maxima, $colObjpasajeros,$refResponsable)
    {
       $this->codigo_viaje = $codigo_viaje;
       $this->destino = $destino;
       $this->cant_maxima = $cant_maxima;
-      $this->arre_pasajero = $arre_pasajero;
+      $this->arre_pasajero = $colObjpasajeros;
+      $this->objResponsable=$refResponsable;
    }
    public function getCodigo_viaje()
    {
@@ -28,10 +30,16 @@ class Viaje
    {
       return $this->cant_maxima;
    }
+   public function getObjResponsable()
+   {
+      return $this->objResponsable;
+   }
+
    public function getArre_pasajero()
    {
       return $this->arre_pasajero;
    }
+   
    /*No definimos set para codigo de viaje, deberia ser clave no modificable*/
    public function setDestino($destino)
    {
@@ -41,67 +49,43 @@ class Viaje
    {
       $this->cant_maxima = $cant_maxima;
    }
+   
+ 
+
+   public function setObjResponsable($objResponsable)
+   {
+      $this->objResponsable = $objResponsable;
+
+   }
    public function setArrePasajero($arre_pas)
    {
 
       $this->arre_pasajero = $arre_pas;
    }
 
-   /*Suponiendo que se pueden modificar todos los datos del pasajero*/
-
-   /**
-    * Modifica el Num. de documento de un pasajero, ubicado por su posicion ($key) en el arreglo o lista de pasajeros. 
-    * @param Int $key
-    * @param String $new_num_documento
-    * @return Void
-    */
-   public function addNumDocumento($key, $new_num_documento)
-   {
-      $this->arre_pasajero[$key]["num_documento"] = $new_num_documento;
-   }
-   /**
-    * Modifica el Nombre de un pasajero, ubicado por su posicion ($key) en el arreglo o lista de pasajeros. 
-    * @param Int $key
-    * @param String $new_nombre
-    * @return Void
-    */
-   public function addNombrePasajero($key, $new_nombre)
-   {
-      $this->arre_pasajero[$key]["nombre"] = $new_nombre;
-   }
-   /**
-    * Modifica el Apellido de un pasajero, ubicado por su posicion ($key) en el arreglo o lista de pasajeros. 
-    * @param Int $key
-    * @param String $new_apellido
-    * @return Void
-    */
-   public function addApellidoPasajero($key, $new_apellido)
-   {
-      $this->arre_pasajero[$key]["apellido"] = $new_apellido;
-   }
-   /**
-    * Verifica si hay espacio en el arreglo o lista de pasajeros, si la cantidad de pasajeros
-    * es menor  a la cantidad maxima, retorna true, sino false
-    * @return Boolean
-    */
+   
    public function verificarEspacio()
    {
       return count($this->getArre_pasajero()) < $this->getCant_maxima();
    }
-   /**
-    * Obtiene los datos del pasajero,, ubicado por su posicion ($key) en el arreglo o lista de pasajeros. 
-    * STRING $num_documento,$nombre,$apellido
-    * @param Int $key
-    * @return String
-    */
-   public function obtenerPasajero($key)
+   function buscarPasajero($num_documento)
    {
-      $num_documento = $this->getArre_pasajero()[$key]["num_documento"];
-      $nombre = $this->getArre_pasajero()[$key]["nombre"];
-      $apellido = $this->getArre_pasajero()[$key]["apellido"];
-
-      return "Num. Documento:" . $num_documento . " Nombre:" . $nombre . " Apellido:" . $apellido . "\n";
-   }
+      $listaPasajeros=$this->getArre_pasajero();
+      $i=0;
+      $buscado=null;
+      $encontrado=false;
+      while($i<count($listaPasajeros)&&(!$encontrado))
+      {       
+         $encontrado=($listaPasajeros[$i]->verificarPasajero($num_documento));
+         if($encontrado)
+         {
+            $buscado=$listaPasajeros[$i];
+         }
+         $i++;     
+      }
+      return $buscado;
+     
+   }   
    /**
     * Agrega un nuevo pasajero a lista o arreglo de pasajeros
     * BOOLEAN $res
@@ -110,12 +94,13 @@ class Viaje
     * @param String $apellido
     * @return Boolean
     */
-   public function agregarPasajero($nombre, $apellido, $num_documento)
+   public function agregarPasajero($nombre, $apellido, $num_documento,$telefono)
    {
       $res = false;
+      $nuevo=new Pasajero($num_documento,$nombre,$apellido,$telefono);
       if ($this->verificarEspacio()) {
          $arre = $this->getArre_pasajero();
-         array_push($arre, ["num_documento" => $num_documento, "nombre" => $nombre, "apellido" => $apellido]);
+         array_push($arre, $nuevo);
          $this->setArrePasajero($arre);
          $res = true;
       }
@@ -133,36 +118,22 @@ class Viaje
       $this->setDestino($destino);
       $this->setCantMaxima($cant_maxima);
    }
-   /**
-    * Actualiza los datos de un pasajero, ubicado por su posicion ($key) en el arreglo o lista de pasajeros. 
-    * INT $cant_pasajero
-    * BOOLEAN $res
-    * @param Int $key
-    * @param String $num_documento
-    * @param String $nombre
-    * @param String $apellido
-    * @return Boolean
-    */
-   public function editarPasajero($key, $num_documento, $nombre, $apellido)
-   {
-      $res = false;
-      $cantPasajero = count($this->getArre_pasajero());
-      if ($key >= 0 && $key < $cantPasajero) {
-         $this->addNumDocumento($key, $num_documento);
-         $this->addNombrePasajero($key, $nombre);
-         $this->addApellidoPasajero($key, $apellido);
-         $res = true;
-      }
-      return $res;
-   }
+
+ /**
+ *  Retorna en forma de cadena de texto los valores de los atributos de la instancia clase ResponsableV.
+ * String cadena
+ * @return String
+ */
+
    public function __toString()
    {
-
       $cadena = "CODIGO DE VIAJE: " . ($this->getCodigo_viaje()) . " DESTINO: " . ($this->getDestino()) . " CANTIDAD MAXIMA: " . ($this->getCant_maxima()) . "\n";
+      $cadena .= "Responsable: ".$this->getObjResponsable()->__toString()."\n";
       $cadena .= "Lista de pasajeros\n";
-      foreach ($this->getArre_pasajero() as $key => $value) {
-         $cadena .= "Num.pasajero:" . ($key + 1) . " Documento:" . $value['num_documento'] . " Nombre:" . $value['nombre'] . " Apellido:" . $value['apellido'] . "\n";
+      foreach ($this->getArre_pasajero() as $pasajero) {
+         $cadena .= $pasajero->__toString(). "\n";
       }
       return $cadena;
    }
+
 }
--- DesarrolloWeb/2022/IPOO/Tpe01/testViaje.php
+++ DesarrolloWeb/2022/IPOO/Tpe01/testViaje.php
@@ -1,10 +1,15 @@
-<?php 
- /*TPE 01 
+<?php
+/*TPE 01 
    Bruno Terrazas FAI-2585*/
-include 'Viaje.php';
+include_once 'Viaje.php';
+include_once 'Pasajero.php';
+include_once 'ResponsableV.php';
 
 /************ PROGRAMA PRINCIPAL***************/
 iniciarTest();
+
+
+
 /************ FIN PROGRAMA PRINCIPAL***************/
 
 /**
@@ -19,25 +24,22 @@ function iniciarTest()
     $salir = false;
     do {
         imprimirMenu();
-    
+
         $opcion = validarNumero("una opcion");
         switch ($opcion) {
             case 0:
-             $salir = true;
-             break;
+                $salir = true;
+                break;
             case 1:
-             $objViaje=cargarViaje();
-             $opcion=menuOpciones($objViaje);
-             break;
-            
+                $objViaje = cargarViaje();
+                $opcion = menuOpciones($objViaje);
+                break;
+
             default:
                 echo "¡Opcion Incorrecta! de nuevo\n";
                 break;
         }
     } while (!$salir);
-    
-    
-
 }
 
 function imprimirMenu()
@@ -45,7 +47,6 @@ function imprimirMenu()
     echo "**********************************************\n";
     echo "(1) Cargar viaje\n";
     echo "Presione (0) o cualquier letra para salir del programa\n";
-
 }
 function imprimirSubMenu()
 {
@@ -53,20 +54,21 @@ function imprimirSubMenu()
     echo " (1) Editar datos del viaje\n";
     echo " (2) Agregar pasajero\n";
     echo " (3) Editar pasajero\n";
-    echo " (4) Mostrar datos del viaje\n";
+    echo " (4) Modificar Responsable del viaje\n";
+    echo " (5) Mostrar datos del viaje\n";
     echo " Presione (0) o cualquier letra para salir del submenu\n";
 }
 /**
  * Muestra el menu de opciones
  * BOOLEAN $salir,$res
- * STRING $num_documento,$nombre,$apellido
+ * STRING $num_documento,$nombre,$apellido,$telefono
  * INT $opcion
  * @param Viaje $objViaje
  * @return Int 
  */
 function menuOpciones(Viaje $objViaje)
 {
-  
+
     $salir = false;
     do {
         imprimirSubMenu();
@@ -80,40 +82,40 @@ function menuOpciones(Viaje $objViaje)
                 modificarViaje($objViaje);
                 break;
             case 2:
-            if($objViaje->verificarEspacio())
-                {
+                if ($objViaje->verificarEspacio()) {
 
-                echo "--------------------/Registrar Pasajero\--------------------\n";   
-                echo "Ingrese numero de documento\n";
-                $num_documento = trim(fgets(STDIN));
-                echo "Ingrese nombre\n";
-                $nombre = trim(fgets(STDIN));
-                echo "Ingrese apellido\n";
-                $apellido = trim(fgets(STDIN));
-                
-                $res=registrarPasajero($objViaje,$num_documento,$nombre,$apellido);
-                if($res)
-                {
-                    echo "'Pasajero registrado correctamente'\n0";
-                }
-                else 
-                    {
+                    echo "--------------------/Registrar Pasajero\--------------------\n";
+
+                    $num_documento = validarPasajero($objViaje);
+                    echo "Ingrese nombre\n";
+                    $nombre = trim(fgets(STDIN));
+                    echo "Ingrese apellido\n";
+                    $apellido = trim(fgets(STDIN));
+                    echo "Ingrese telefono\n";
+                    $telefono = trim(fgets(STDIN));
+
+                    $res = registrarPasajero($objViaje, $num_documento, $nombre, $apellido, $telefono);
+                    if ($res) {
+                        echo "'Pasajero registrado correctamente'\n0";
+                    } else {
                         echo "'No se pudo registrar'\n";
                     }
-            }
-            else
-               {          
-                echo  "'No hay espacio disponible'\n";
-               }
-            break;
+                } else {
+                    echo  "'No hay espacio disponible'\n";
+                }
+                break;
             case 3:
-                echo "*******************DATOS DEL VIAJE*******************\n".$objViaje;
+                echo "*******************DATOS DEL VIAJE*******************\n" . $objViaje;
                 modificarPasajero($objViaje);
                 break;
             case 4:
-                echo "*******************DATOS DEL VIAJE*******************\n".$objViaje;
+                echo "*******************DATOS DEL VIAJE*******************\n" . $objViaje;
+                modificarResponsable($objViaje);
                 break;
-          
+            case 5:
+                echo "*******************DATOS DEL VIAJE*******************\n" . $objViaje;
+                break;
+
 
             default:
                 echo "¡Opcion Incorrecta!, de nuevo\n";
@@ -122,133 +124,167 @@ function menuOpciones(Viaje $objViaje)
     } while (!$salir);
 
     return $opcion;
-}  
+}
 
 /**
-     * Crea un viaje y carga sus datos
-     * INT $cant_maxima
-     * STRING $codigo_viaje
-     * STRING $destino
-     * ARRAY $listapasajero
-     * @return Viaje
-     */
+ * Crea un viaje y carga sus datos
+ * INT $cant_maxima
+ * STRING $codigo_viaje
+ * STRING $destino
+ * ARRAY $listapasajero
+ * @return Viaje
+ */
 function cargarViaje()
 {
+    $unResponsableV = new ResponsableV(1, 234234, "Julio", "Cruz");
     $listapasajero = array();
-    $listapasajero[0] = ["num_documento" => "2323222", "nombre" => "Enzo", "apellido" => "Perez"];
-    $listapasajero[1] = ["num_documento" => "5423343", "nombre" => "Cesar", "apellido" => "Martinez"];
-    $listapasajero[2] = ["num_documento" => "3303332", "nombre" => "Mario", "apellido" => "Sosa"];
-    $listapasajero[3] = ["num_documento" => "5533233", "nombre" => "Antonella", "apellido" => "Ledezma"];
-    $listapasajero[4] = ["num_documento" => "2323222", "nombre" => "Xavier", "apellido" => "Hernandez"];
-    $listapasajero[5] = ["num_documento" => "1332333", "nombre" => "Antonio", "apellido" => "Olmos"];
-    $listapasajero[6] = ["num_documento" => "1322222", "nombre" => "Nicolas", "apellido" => "Nuñez"];
-    $listapasajero[7] = ["num_documento" => "5555533", "nombre" => "Lara", "apellido" => "Mendez"];
-    $codigo_viaje="v01";
-    $destino="Bariloche";
-    $cant_maxima=10;
-   
- 
-    $objViaje= new Viaje($codigo_viaje,$destino,$cant_maxima,$listapasajero);
-    
-    return $objViaje;
+    $listapasajero[0] = new Pasajero("2323222", "Enzo", "Perez", "2991542323");
+    $listapasajero[1] = new Pasajero("4323222", "Facundo", "Rosales", "292542323");
+    $listapasajero[2] = new Pasajero("1323222", "Pablo", "Perez", "2995532322");
+    $listapasajero[3] = new Pasajero("6323222", "Carlos", "Nuñez", "0115423444");
+    $listapasajero[4] = new Pasajero("7323222", "Pedro", "Solari", "2903333323");
+    $listapasajero[5] = new Pasajero("3333222", "Raul", "Gonzalez", "297772323");
+    $listapasajero[6] = new Pasajero("3443222", "Maria", "Rojas", "2995423881");
+    $listapasajero[7] = new Pasajero("2366622", "Esteban", "Lopez", "0115314299");
+    $codigo_viaje = "v01";
+    $destino = "Bariloche";
+    $cant_maxima = 10;
+    $objViaje = new Viaje($codigo_viaje, $destino, $cant_maxima, $listapasajero,$unResponsableV);
 
+    return $objViaje;
 }
 /**
-     * Permite registrar un nuevo pasajero 
-     * INT $new_cant_maxima
-     * STRING $new_destino
-     * @param Viaje $objViaje
-     * @return Void
-     */
+ * Permite modificar al responsable del viaje
+ * INT $num_responsable,$numlicencia
+ * STRING $nombre,apellido
+ * @param Viaje $objViaje
+ * @return Void
+ */
+function modificarResponsable(Viaje $objViaje)
+{
+    echo $objViaje;
+    echo "---------------------/Modificar responsable de viaje\---------------------\n";
+    echo "Ingrese Num. Responsable\n";
+    $num_responsable = trim(fgets(STDIN));
+    echo "Ingrese Nº licencia\n";
+    $num_licencia = trim(fgets(STDIN));
+    echo "Ingrese nombre\n";
+    $nombre = trim(fgets(STDIN));
+    echo "Ingrese apellido\n";
+    $apellido = trim(fgets(STDIN));
+    $objViaje->setObjResponsable(new ResponsableV($num_responsable, $num_licencia, $nombre, $apellido));
+    echo "Responsable modificado Correctamente";
+}
+/**
+ * Permite registrar un nuevo pasajero 
+ * INT $new_cant_maxima
+ * STRING $new_destino
+ * @param Viaje $objViaje
+ * @return Void
+ */
 function modificarViaje(Viaje $objViaje)
 {
-     echo $objViaje;
-     echo "-----------------/Editar Viaje\----------------\n";
-     echo "Ingrese nuevo destino\n";
-     $new_destino = trim(fgets(STDIN));
-     $new_cant_maxima = validarNumero("cantidad maxima");
-     while($new_cant_maxima<count($objViaje->getArre_pasajero()))
-     {
-        $new_cant_maxima = validarNumero("otra cantidad maxima");     
-     }
-    $objViaje->editarViaje($new_destino,$new_cant_maxima);
-}
-/**
-     * Permite registrar un nuevo pasajero 
-     * INT $num_pasajero
-     * BOOLEAN $res
-     * @param Viaje $objViaje
-     * @param String $num_documento
-     * @param String $nombre
-     * @param String $apellido
-     * @return Boolean
-     */
-function registrarPasajero(Viaje $objViaje,$num_documento,$nombre,$apellido)
-{   
-    $cantPasajeros=count($objViaje->getArre_pasajero());
-    $res=false;
-    if($cantPasajeros<$objViaje->getCant_maxima())
-    {
-    $res=$objViaje->agregarPasajero($nombre,$apellido,$num_documento);
+    echo $objViaje;
+    echo "-----------------/Editar Viaje\----------------\n";
+    echo "Ingrese nuevo destino\n";
+    $new_destino = trim(fgets(STDIN));
+    $new_cant_maxima = validarNumero("cantidad maxima");
+    while ($new_cant_maxima < count($objViaje->getArre_pasajero())) {
+        $new_cant_maxima = validarNumero("otra cantidad maxima");
     }
-   
+    $objViaje->editarViaje($new_destino, $new_cant_maxima);
+}
+/**
+ * Permite registrar un nuevo pasajero 
+ * INT $num_pasajero
+ * BOOLEAN $res
+ * @param Viaje $objViaje
+ * @param String $num_documento
+ * @param String $nombre
+ * @param String $apellido
+ * @param String $telefono
+ * @return Boolean
+ */
+function registrarPasajero(Viaje $objViaje, $num_documento, $nombre, $apellido, $telefono)
+{
+    $cantPasajeros = count($objViaje->getArre_pasajero());
+    $res = false;
+    if (($objViaje->buscarPasajero($num_documento) == null)) {
+        if ($cantPasajeros < $objViaje->getCant_maxima()) {
+            $res = $objViaje->agregarPasajero($nombre, $apellido, $num_documento, $telefono);
+        }
+    }
     return $res;
-     
 }
 
 
 /**
-     * Permite editar los datos del pasajero 
-     * INT $num_pasajero
-     * STRING $num_documento,$nombre,$apellido
-     * BOOLEAN $res
-     * @param Viaje $objViaje
-     * @return Void
-     */
-    function modificarPasajero(Viaje $objViaje)
-    {
-       echo $objViaje;
-       $num_pasajero = validarNumero("Num. de pasajero");
-       //verificamos si el numero de pasajero esta en la lista de pasajeros, sino vuelve a ingresar el num. pasajero
-       while(!(($num_pasajero-1)>=0&&($num_pasajero-1)<count($objViaje->getArre_pasajero())))
-       {
-        $num_pasajero = validarNumero("Num. de pasajero");       
-       }
-       echo $objViaje->obtenerPasajero($num_pasajero-1); 
-       echo "---------------------/Editar Pasajero\---------------------\n";  
-       echo "Ingrese numero de documento\n";
-       $num_documento = trim(fgets(STDIN));
-       echo "Ingrese nombre\n";
-       $nombre = trim(fgets(STDIN));
-       echo "Ingrese apellido\n";
-       $apellido = trim(fgets(STDIN));
-       $res=$objViaje->editarPasajero($num_pasajero-1,$num_documento,$nombre,$apellido);
-       if($res)
-       {
-           echo "'Actualizado correctamente'\n";
-       }
-       else
-          echo "'No se puedo actualizar'\n";
-
-    }  
-
+ * Permite editar los datos del pasajero 
+ * STRING $num_documento,$nombre,$apellido
+ * BOOLEAN $res
+ * @param Viaje $objViaje
+ * @return Void
+ */
+function modificarPasajero(Viaje $objViaje)
+{
+    echo "Ingrese numero de documento\n";
+    $num_documento = trim(fgets(STDIN));
+    $objPasajero = $objViaje->buscarPasajero($num_documento);;
+    while ($objPasajero == null) {
+        echo "Ingrese numero de documento\n";
+        $num_documento = trim(fgets(STDIN));
+        $objPasajero = $objViaje->buscarPasajero($num_documento);
+    }
+    echo $objPasajero;
+    echo "---------------------/Editar Pasajero\---------------------\n";
+    echo "Ingrese nombre\n";
+    $nombre = trim(fgets(STDIN));
+    echo "Ingrese apellido\n";
+    $apellido = trim(fgets(STDIN));
+    echo "Ingrese telefono\n";
+    $telefono = trim(fgets(STDIN));
+    $res = $objPasajero->actualizarDatos($nombre, $apellido, $telefono);
+    if ($res) {
+        echo "'Actualizado correctamente'\n";
+    } else
+        echo "'No se puedo actualizar'\n";
+}
 /**
-     * Valida que sea un numero, solo si se cumple, retorna ese valor 
-     * FLOAT $valor
-     * BOOLEAN $res
-     * @param $texto
-     * @return Int
-     */
-    function validarNumero($texto)
-    {
+ * Valida que el pasajero no esta registrado en el viaje, considerando su dni, 
+ * retorna el numero de documento si no esta registrado 
+ * STRING $num_documento,$nombre,$apellido
+ * BOOLEAN $res
+ * @param Viaje $objViaje
+ * @return Pasajero
+ */
+function validarPasajero($objViaje)
+{
+    echo "Ingrese numero de documento\n";
+    $num_documento = trim(fgets(STDIN));
+    while (($objViaje->buscarPasajero($num_documento) != null)) {
+        echo "Ya esta registrado en este viaje, por favor:\n";
+        echo "Ingrese otro numero de documento\n";
+        $num_documento = trim(fgets(STDIN));
+    }
+    return $num_documento;
+}
+/**
+ * Valida que sea un numero, solo si se cumple, retorna ese valor 
+ * FLOAT $valor
+ * BOOLEAN $res
+ * @param $texto
+ * @return Int
+ */
+function validarNumero($texto)
+{
 
-        echo "Ingrese " . $texto . "\n";
+    echo "Ingrese " . $texto . "\n";
+    $valor = trim(fgets(STDIN));
+    $res = is_numeric($valor);
+    while (!$res) {
+        echo "¡Error! Ingrese " . $texto . "\n";
         $valor = trim(fgets(STDIN));
         $res = is_numeric($valor);
-        while (!$res) {
-            echo "¡Error! Ingrese " . $texto . "\n";
-            $valor = trim(fgets(STDIN));
-            $res = is_numeric($valor);
-        }
-        return $valor;
     }
+    return $valor;
+}
